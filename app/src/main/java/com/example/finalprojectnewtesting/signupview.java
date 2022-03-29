e96e516217203fefa2fbbf6e9eb8aade9fcc73ae
Naman Rai <namanrai306@gmail.com>
2022-03-29 12:01:51 -0400

    added login, signup and covid activity

---
+++ app/src/main/java/com/example/finalprojectnewtesting/signupview.java
@@ -0,0 +1,14 @@
+package com.example.finalprojectnewtesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signupview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signupview);
+    }
+}
