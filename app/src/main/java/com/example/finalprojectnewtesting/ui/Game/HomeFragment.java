65803221939091c819f270ae39a097d57f5878e8
Naman Rai <namanrai306@gmail.com>
2022-04-01 07:42:24 -0400

    added new activities for gameplay view, gameplay mode selection, leaderboard/score view. tried removing the name from signup but it was messing with code in the class. so yeah..i am gonna go sleep for a while

--- app/src/main/java/com/example/finalprojectnewtesting/ui/Game/HomeFragment.java
+++ app/src/main/java/com/example/finalprojectnewtesting/ui/Game/HomeFragment.java
@@ -27,15 +27,19 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
+
+
     }
+
+
 
     @Override
     public void onDestroyView() {
