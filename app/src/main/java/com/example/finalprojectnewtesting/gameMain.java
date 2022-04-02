106eeebcb107369ffd7d981893fd8c0f14115d18
Naman Rai <namanrai306@gmail.com>
2022-04-02 00:57:35 -0400

    made new activity for gameplay, fixed homefragment, added sidebar item

--- app/src/main/java/com/example/finalprojectnewtesting/gameMain.java
+++ app/src/main/java/com/example/finalprojectnewtesting/gameMain.java
@@ -35,7 +35,7 @@ public class gameMain extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_leader,  R.id.nav_covid)
+                R.id.nav_covid, R.id.nav_home, R.id.nav_leader, R.id.nav_game)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_game_main);
---
+++ app/src/main/java/com/example/finalprojectnewtesting/ui/gamePlayNew.java
@@ -0,0 +1,16 @@
+package com.example.finalprojectnewtesting.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalprojectnewtesting.R;
+
+public class gamePlayNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_play_new);
+    }
+}
